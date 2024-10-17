8e944b50ddf1bdbf5de7133e1a41850bc27063be
Park Hyewon <db1648@naver.com>
2024-10-17 16:33:20 +0900

    241017 스프링 부트와 jpa 환경설정

---
+++ src/main/java/com/example/tilboard/TilboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.tilboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tilboard/TilboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tilboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
